df03d104470aca70c9bed32644f6ed1732654fbf
anniqueaarts <anniqueaarts@g.ucla.edu>
2026-02-07 22:28:14 -0800

    voice commands n led integration

--- ConnectingPi/pi1_agent.py
+++ ConnectingPi/pi1_agent.py
@@ -32,6 +32,7 @@ from collections import deque
 import asyncio
 import paho.mqtt.client as mqtt
 import numpy as np
+import pyaudio
 
 # Voice mapping (your existing module)
 import voice_commands as vc
